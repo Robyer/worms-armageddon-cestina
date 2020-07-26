1e4b2528f9b9bf54e575a0e4babc7ae395cfee63
Robert Pösel <robyer@seznam.cz>
2020-07-26 19:08:22 +0200

    Reorganize folders

---
+++ _old/orig/english_stripped.h
@@ -0,0 +1,807 @@
+
+
+
+
+
+
+
+
+"Let's catch that one again!"
+"Let's take another look..."
+"In case you missed it..."
+"Incredible... replay coming up"
+"That deserves a second showing..."
+"We got that one on tape!"
+"Watch closely..."
+"See how it's done..."
+"Blink and you'll miss it..."
+"The action is really heating up!"
+"That deserves another look"
+
+
+
+
+
+
+
+
+
+"A weapon crate!"
+"A jewel from the heavens!"
+"A gift from the Gods!"
+"Party time!"
+"This ain't no packed lunch!"
+"I wonder what it contains?"
+"What's in the box?"
+"Quick! A weapon drop!"
+"First come, first served"
+"Lucky, lucky, lucky!"
+"Christmas comes early!"
+"Is it your birthday?"
+"How fortunate!"
+"Cool things come in small packages"
+"Someone likes you..."
+"Collect and destroy!"
+"Gift wrapped and ready to rock!"
+"It's raining weapons!"
+"Can we tempt you with this?"
+
+
+
+
+
+
+
+
+
+"Reinforcements arrive for %s"
+"%s receives reinforcements"
+"Back-up has arrived for %s"
+"%s armoury is extended"
+
+
+
+
+
+
+
+
+
+"!Go for it %s"
+"!%s smells victory"
+"!%s boldly steps up"
+"!%s is ready for action"
+"!%s is ready to rumble"
+"!Get'em %s"
+"!Here comes %s"
+"!%s is up"
+"!Time for %s"
+"!%s gets ready"
+"!%s is next"
+"!%s contemplates the move"
+"!%s surveys the scene"
+"!%s plans the next move"
+"!%s ponders the next move"
+"!%s is on the move..."
+"!%s hatches a master plan..."
+
+
+
+
+
+
+
+
+
+"Well done %s"
+"Victory for %s!"
+"%s wins the round!"
+"%s. Numero Uno!"
+"Congratulations to %s!"
+
+
+
+
+
+
+
+
+
+"%s is an ex-worm"
+"%s joins the great worm in the sky"
+"%s is history"
+"%s is dead"
+"%s is no more"
+"Poor %s"
+"Awww, poor %s"
+"%s is 6 feet under"
+"%s is dead and buried"
+"%s shall be missed"
+"%s has gone"
+"%s has been wasted"
+"%s has turned up his toes"
+"%s could take no more"
+"%s bit the dust"
+"%s takes a dirt nap"
+"%s is pushing up the daisies"
+"%s wasn't hard enough"
+"%s didn't make it"
+"Have pity for %s"
+"%s has kicked the bucket"
+"%s has passed away"
+"%s has ceased to be"
+"%s met their demise"
+"%s is mourned"
+"Shed a tear for poor %s"
+"%s deserved it"
+"%s was asking for it"
+"%s has been terminated"
+"%s is gone, but not forgotten"
+"%s was rubbed out"
+"%s was eradicated"
+"%s was no use anyway"
+"%s is donated to medical science"
+"Our sympathy for %s"
+"So long %s"
+"It was nice knowing %s"
+"%s waved goodbye"
+"%s is knocking on heavens door"
+
+
+
+
+
+
+
+
+
+"%s fade away"
+"%s is out!"
+"%s is out of here!"
+"%s bit the dust"
+"%s didn't make the grade"
+"%s bit the bullet"
+"%s is missing in action"
+"%s is history"
+"It's too late for %s"
+"Have pity for %s"
+
+
+
+
+
+
+
+
+
+"Game Drawn"
+"What a boring outcome - all dead!"
+"Yawn... A draw."
+"Draw! All that, for nothing!"
+"A draw! You'll have to do it all again!"
+"There's no-one left to claim a victory!"
+"Completely inconclusive!"
+"A thoroughly dull draw!"
+"Hope you've got the energy for the re-match!"
+"A draw. How dull."
+
+
+
+
+
+
+
+
+
+"%s has drowned"
+"%s has slipped off the hook"
+"%s is sleeping with the fishies"
+"%s couldn't swim"
+"%s sank like the Titanic"
+"%s lies on the bottom of the sea"
+"%s went in the drink"
+"%s took a nose dive"
+"%s is sleeping on the sea bed"
+"%s swam like a brick"
+"Water way to go, %s"
+
+
+
+
+
+
+
+
+
+"SUDDEN DEATH MODE ON!"
+"Make 'em pay! SUDDEN DEATH!"
+"It's time for... SUDDEN DEATH!"
+"Time out! SUDDEN DEATH ENGAGED!"
+"Careful out there, it's SUDDEN DEATH!"
+"Tactics out of the window, it's SUDDEN DEATH!"
+
+
+
+
+
+
+
+
+
+""_LANG_STRING_" makes it count"
+""_LANG_STRING_" hands out some punishment"
+""_LANG_STRING_" adds a notch"
+""_LANG_STRING_" plays it cool"
+""_LANG_STRING_" wastes no time, only worms"
+""_LANG_STRING_" wastes one"
+""_LANG_STRING_" shows no remorse"
+"Watch out, "_LANG_STRING_" is about"
+""_LANG_STRING_" does the damage"
+""_LANG_STRING_" turns the heat up"
+""_LANG_STRING_" deals death"
+""_LANG_STRING_" insists it's personal"
+""_LANG_STRING_" marks the spot"
+""_LANG_STRING_" tests the water"
+""_LANG_STRING_" takes one out"
+"Easy as pie for "_LANG_STRING_""
+""_LANG_STRING_" picks one off"
+""_LANG_STRING_" makes a judgement"
+
+
+
+
+
+""_LANG_STRING_" scores a double kill!"
+"Twice as nice for "_LANG_STRING_""
+""_LANG_STRING_" uses the old one-two"
+"Double dose from "_LANG_STRING_""
+"Nice pair from "_LANG_STRING_""
+""_LANG_STRING_" pockets a twin-pack"
+""_LANG_STRING_" gets 2 for the price of 1"
+""_LANG_STRING_" Doubles up"
+"Twinkill combo for "_LANG_STRING_""
+"Nice combo by "_LANG_STRING_""
+"Two notch combo by "_LANG_STRING_""
+
+
+
+
+
+""_LANG_STRING_" scores a 3 kill combo!"
+"Easy as 1-2-3 for "_LANG_STRING_""
+"Three and easy does it, "_LANG_STRING_""
+"3 for the price of 1!"
+"Nice threesome by "_LANG_STRING_""
+""_LANG_STRING_" wastes 3!"
+""_LANG_STRING_" cuts a 3-pack"
+""_LANG_STRING_" cuts loose with a 3-way split!"
+"3-fall for "_LANG_STRING_"!"
+
+
+
+
+
+""_LANG_STRING_" grabs an amazing 4-kill combo!"
+"4-Combo by "_LANG_STRING_"!"
+"1-2-3-4 Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" has a blast & pockets 4!"
+"Did you see that? 4 Kills!"
+
+
+
+
+
+""_LANG_STRING_" grabs an amazing 5-kill combo!"
+"5-Combo by "_LANG_STRING_"!"
+"FIVE Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" has a blast and gets 5!"
+"5!! Don't mess with "_LANG_STRING_"!"
+"5 Kills, did you see that?"
+
+
+
+
+
+""_LANG_STRING_", like a six machine!"
+"Super 6-Salvo by "_LANG_STRING_"!"
+"Can you believe it! 6 Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" has a blast and gets 6!"
+"6!! Don't mess with "_LANG_STRING_"!"
+"6 Kills, did you see that?"
+""_LANG_STRING_" grabs a 6-pack!"
+""_LANG_STRING_" has six on the brain!"
+
+
+
+
+
+"Lucky 7 for "_LANG_STRING_""
+"Super 7-Salvo by "_LANG_STRING_"!"
+"Can you believe it! 7 Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" has a blast and gets 7!"
+"7th heaven for "_LANG_STRING_""
+"7 Kills, did you see that?"
+
+
+
+
+
+"Incredible 8 kill combo!"
+"Super 8-Salvo by "_LANG_STRING_"!"
+"Can you believe it! 8 Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" has a blast and does 8!"
+"8 Amazing Kills, did you see that?"
+""_LANG_STRING_" grabs 8!"
+
+
+
+
+
+"Incredible 9 kill combo!"
+"Super 9-Salvo by "_LANG_STRING_"!"
+"Can you believe it! 9 Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" has a blast and does 9!"
+"9 Amazing Kills, did you see that?"
+""_LANG_STRING_" grabs 9!"
+"The cat is wasted! 9 lives gone!"
+"Amazing 9 kill salvo!"
+
+
+
+
+
+"10 times the fun for "_LANG_STRING_""
+""_LANG_STRING_" gets a top 10 hit!"
+"Can you believe it! 10 Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" grabs a 10pack!"
+
+
+
+
+
+"Incredible 11 kill combo!"
+"Super 11-Salvo by "_LANG_STRING_"!"
+"Can you believe it! 11 Kills by "_LANG_STRING_"!"
+""_LANG_STRING_" has a blast and does 11!"
+"11 Amazing Kills, did you see that?"
+""_LANG_STRING_" grabs 11!"
+
+
+
+
+
+"Stunning-12-kill-combo by "_LANG_STRING_"!!"
+"Super 12-Salvo by "_LANG_STRING_"!"
+"Can you believe it! 12 Kills by "_LANG_STRING_"!"
+"12 Amazing Kills, did you see that?"
+
+
+
+
+
+"A baker's dozen for "_LANG_STRING_"!!"
+"Unlucky for some! 13 kills for "_LANG_STRING_"!"
+"13 Amazing Kills, did you see that?"
+
+
+
+
+
+"Max "_LANG_NUMBER_" super-kill-combo by "_LANG_STRING_"!!"
+"Super "_LANG_NUMBER_"-Salvo by "_LANG_STRING_"!"
+"Can you believe it! "_LANG_NUMBER_" Kills by "_LANG_STRING_"!"
+"That's got to be a record! "_LANG_NUMBER_" Kills!"
+""_LANG_NUMBER_" Amazing Kills, "_LANG_STRING_" has to be hornswoggling!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"Not long left..."
+"The clock is ticking"
+"Time is ticking away..."
+"Hurry... not long to go"
+"There's not much left on the clock..."
+"Better hurry... Sudden death looms!"
+
+
+
+
+
+
+
+"!*** NO Shots Remaining ***"
+"!1 Shot Remaining"
+"!%d Shots Remaining"
+
+
+
+"Waiting for other Players"
+"%s - forced out due to network failure!"
+"*Whisper*"
+"Searching for Network : %d"
+"First Team to %d Wins"
+"Minimize Game"
+"Volume"
+"Force Sudden Death"
+"Draw This Round"
+"Quit The Game"
+"Are You Sure?"
+"YES"
+"NO"
+
+
+
+"MIN Bounce" // 'Bounce as in 'Boing'
+"MAX Bounce" // 'Bounce as in 'Boing'
+"Herd"             // 'Herd' as in a Herd of Cows
+"Sec"               // 'Seconds'
+
+
+
+"Bazooka"
+"Homing Missile"
+"Mortar"
+"Grenade"
+"Cluster Bomb"
+"Banana Bomb"
+"Holy Grenade"
+"Petrol Bomb"
+"Shotgun"
+"Handgun"
+"Uzi"
+"Minigun"
+"Fire Punch"
+"Dragon Ball"
+"Kamikaze"
+"Dynamite"
+"Mine"
+"Ming Vase"
+"Carpet Bomb"
+"Donkey"
+"Air Strike"
+"Homing Strike"
+"Napalm Strike"
+"Mail Strike"
+"Girder"
+"Pneumatic Drill"
+"Baseball Bat"
+"Prod"
+"Teleport"
+"Ninja Rope"
+"Bungee"
+"Surrender"
+"Skip Go"
+"Parachute"
+"Super Banana"
+"MB Bomb"
+"Sheep"
+"Cloned Sheep"
+"Mad Cow"
+"Old Woman"
+"Salvation Army"
+"Super Sheep"
+"Homing Pigeon"
+"BlowTorch"
+"Magic Bullet"
+
+"Bazooka"
+"Homing Missile"
+"Mortar"
+"Grenade"
+"Cluster Bomb"
+"Banana Bomb"
+"Holy Hand-Grenade"
+"Petrol Bomb"
+"Shotgun"
+"Handgun"
+"Uzi"
+"Minigun"
+"Fire Punch"
+"Dragon Ball"
+"Kamikaze"
+"Dynamite"
+"Mine"
+"Priceless Ming Vase"
+"Mike's Carpet Bomb"
+"Concrete Donkey"
+"Air Strike"
+"Homing Air Strike"
+"Napalm Strike"
+"Mail Strike"
+"Girder"
+"Pneumatic Drill"
+"Baseball Bat"
+"Prod"
+"Teleport"
+"Ninja Rope"
+"Bungee"
+"Surrender"
+"Skip Go"
+"Parachute"
+"Super Banana Bomb"
+"MB Bomb"
+"Sheep"
+"Cloned Sheep"
+"Mad Cow"
+"Old Woman"
+"Salvation Army"
+"Super Sheep"
+"Homing Pigeon"
+"BlowTorch"
+"Patsy's Magic Bullet"
+
+
+
+
+
+
+
+"%s leaves the family jewels behind..."
+"And the geek shall inherit the earth..."
+"%s drops a Donor card!"
+"%s makes a donation."
+"%s makes a gesture to the kitty!"
+"%s leaves an airloom"
+"The legacy of %s!"
+"Finders Keepers!"
+
+
+
+
+
+
+
+
+
+"Get ready to rumble!"
+"Are you ready?"
+"Training about to commence..."
+"Remember, practice makes perfect!"
+"You can only get better!"
+"Show us what you can do!"
+"Give it some welly!"
+"Quick, while no-ones watching..."
+"How good are you?"
+
+
+
+
+
+
+
+
+
+"Fallout!"
+"The air is poisoned!"
+"Sickness prevails..."
+"Now it's only a matter of time..."
+"What pretty snowflakes..."
+"Cough drops!"
+"Radiation sickness descends."
+
+
+
+
+
+
+
+
+
+"%s considers it even!"
+"%s balances the odds."
+"%s it's honours even."
+"%s readdresses the balance."
+"%s settles the scores."
+"Even Stevens."
+
+
+
+
+
+
+
+
+
+"Home Run!"
+"It's outta here!"
+"That's got to hurt!"
+"Bases loaded!"
+"Strike!"
+"That's a perfect home run!"
+
+
+
+
+
+
+
+
+
+"Tools for fools."
+"DIY supplement."
+"Utility dispensed."
+"Special tools ahoy!"
+"Coo! That looks useful!"
+"Spanner Mana."
+"Interesting..."
+"You might find this useful."
+"Top tools..."
+"Collect or conquer?"
+
+
+
+
+
+
+
+
+
+"Certainly a healthy option..."
+"Dr Davidson's cure-all."
+"Grab it while you can."
+"Collect or save, the choice is yours."
+"Energy bundle!"
+"This might save your bacon..."
+"Red cross, worm happy."
+"Energy boost!"
+"Very tempting..."
+
+
+
+
+
+
+
+
+
+"%s is bug ridden."
+"%s feels under the weather."
+"%s has a fever."
+"%s is a bio-hazard."
+"%s is sick."
+"%s is stricken."
+"%s is poorly."
+"%s is ill."
+"%s needs medicine."
+"%s is feeling a bit dodgy."
+"%s has a virus."
+"%s needs to see a doctor."
+"%s is suffering."
+"%s needs medical attention."
+"%s needs nursing."
+"%s has the dreaded lurgie."
+"%s is feeling ropey."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"Flame Thrower"
+"Mine Strike"
+"Select Worm"
+"Aqua Sheep"
+"Skunk"
+"Longbow"
+"Battle Axe"
+"Crate Spy"
+"Fast Walk"
+"Laser Sight"
+"Damage*2"
+"Invisibility"
+"Low Gravity"
+"Sheep Launcher"
+"Girder Pack"
+"Freeze"
+"Earth Quake"
+"Scales of Justice"
+"Worm Strike"
+"Sheep Strike"
+"Nuclear Test"
+"Jet Pack"
+"Double Time"
+"Crate Shower"
+"Mole Bomb"
+"Suicide Bomber"
+"Armageddon"
+"Mole Squadron"
+
+"Flame Thrower"
+"Mine Strike"
+"Select Worm"
+"Aqua Sheep"
+"Skunk"
+"Longbow"
+"Battle Axe"
+"Sheep Launcher"
+"Crate Spy"
+"Fast Walk"
+"Laser Sight"
+"Damage*2"
+"Invisibility"
+"Low Gravity"
+"Girder Starter-Pack"
+"Freeze"
+"Earth Quake"
+"Scales of Justice"
+"Worm Strike"
+"French Sheep Strike"
+"Indian Nuclear Test"
+"Jet Pack"
+"Double Turn Time"
+"Crate Shower"
+"Mole Bomb"
+"Suicide Bomber"
+"Armageddon"
+"Mole Squadron"
+
+"Waiting for Players - %d"
+"Invalid Function-Key"
+
+"unknown command"
+"unknown machine"
+
+"no message specified"
+"no machine specified"
+
+"Util."
+
+
